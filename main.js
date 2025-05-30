7d63a5a5ea51d16a2a7ba80e943b8e829b58b26e
Timon <fitform163@mail.ru>
2025-05-30 23:44:12 +0300

    pravka

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-// задача 2
+// Задача 1: Объект coffeeMachine
+
+let coffeeMachine = {
+    water: 0,
+    addwater(addwaterX) {
+        this.water = this.water + addwaterX;
+        alert("для приготовления кофе идет наполнение воды")
+    },
+    makeCoffee() {
+        if (this.water >= 100) {
+            this.water = this.water - 100;
+            alert("идёт приготовление одной чашки кофе")
+        } else {
+            alert("для приготовления не хватает воды")}
+    },
+    showWater () {
+       alert(`остаток воды в кофемашине после приготовления кофе ${this.water} мл`);
+    }  
+}
+coffeeMachine.addwater(150)
+coffeeMachine.makeCoffee()
+coffeeMachine.showWater()
+
+
+
+
+// Задача 2.
+
 
 function microwave(foodName, seconds) {
     this.putFood = foodName;
@@ -6,28 +33,13 @@ function microwave(foodName, seconds) {
 
     this.start = function() {
         alert(`Подогреваем ${this.putFood} ${this.setTime} секунд!`)
-    }
+    } 
+    if (foodName = null   || seconds == 0) {
+    alert("Ошибка") 
+} 
 }
-// if (foodName = null || seconds == 0) {
-//     alert("Ошибка")
-// }
-let food = new microwave("фуагра","95")
+
+let food = new microwave("фуагра", "95")
 food.start()
+console.log(food)
 
-// задача 1
-
-let coffeeMachine = {
-    water: 0,
-    addwater() {
-        this.water++;
-        return this
-    },
-    makeCoffee() {
-        this.water-100;
-        return this
-    },
-    showWater function() {
-        alert(this.water);
-        return this
-    }
-}
